0287473a2c6d08f49f74b9af019f2f814f0e740c
EC2 Default User <ec2-user@ip-10-11-1-32.us-east-2.compute.internal>
2019-10-21 05:13:00 +0000

    Data can be sent and recieved from front-end, back-end, and database

---
+++ api/routes/config.js
@@ -0,0 +1,9 @@
+let config = {
+  host : 'database-1.csbhhsidcs3g.us-east-2.rds.amazonaws.com',
+  port : 3306,
+  user : 'admin',
+  password : 'bohlmannsbohlmanns',
+  database : 'main_site'
+};
+
+module.exports = config;
--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
-router.get('/', function(req, res, next) {
-    res.send('API is working properly');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+router.post('/', urlencodedParser, function(req, res, next) {
+    console.log('Logging request body');
+    console.log(req.body.username);
+    res.send('API has recieved: ' + req.body);
 });
 
 module.exports = router;
