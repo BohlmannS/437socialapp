0126d2a936927b88025682d008045b5df84295a8
Juila Li <lijulia@wustl.edu>
2019-12-09 16:39:48 -0600

    ref fix

--- api/public/js/messaging.js
+++ api/public/js/messaging.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
         // });
 
         const preObject = document.getElementById('object');
-        const dbRefObject = firebase.database().ref.child('object');
+        const dbRefObject = firebase.database().ref().child('object');
         dbRefObject.on('value', snap => console.log(snap.val()));
         console.log("send button clicked");
         // var testUser = "test";
