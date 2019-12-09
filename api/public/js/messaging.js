35a61062ad6914c1a51ffbe7b1f06e36dd2a4c05
Juila Li <lijulia@wustl.edu>
2019-12-09 15:35:46 -0600

    try button click

--- api/public/js/messaging.js
+++ api/public/js/messaging.js
@@ -1,39 +1,50 @@
 $(document).ready(function () {
 
-    var myFirebase = new Firebase('https://fir-messaging-app-86905.firebaseio.com/');
+    var firebaseConfig = {
+        apiKey: "AIzaSyCygtuUxUGItCvV7UkDRCHrnBjjQQQnP8o",
+        authDomain: "fir-messaging-app-86905.firebaseapp.com",
+        databaseURL: "https://fir-messaging-app-86905.firebaseio.com",
+        projectId: "fir-messaging-app-86905",
+        storageBucket: "fir-messaging-app-86905.appspot.com",
+        messagingSenderId: "812660206927",
+        appId: "1:812660206927:web:c6a0690980ebdae4ce293a",
+        measurementId: "G-J72MYFVE6Y"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
     var textInput = document.querySelector(".write_msg");
     var postButton = document.querySelector(".msg_send_btn");
 
     postButton.addEventListener("click", function () {
-        var msgText = textInput.value;
-        myFirebase.set(msgText);
-        textInput.value = "";
+        // var msgText = textInput.value;
+        // myFirebase.set(msgText);
+        // textInput.value = "";
 
         var testUser = "test";
 
-        myFirebase.push({ name: testUser, sender_id: "1234", text: msgText });
+        // myFirebase.push({ name: testUser, sender_id: "1234", text: msgText });
     });
 
-    var beginListening = function () {
-        myFirebase.on('child_added', function (snapshot) {
-            var msg = snapshot.val();
+    // var beginListening = function () {
+    //     myFirebase.on('child_added', function (snapshot) {
+    //         var msg = snapshot.val();
 
-            // var msgUsernameElement = document.createELement("b");
-            // msgUsernameELement.textContent = msg.name;
+    //         // var msgUsernameElement = document.createELement("b");
+    //         // msgUsernameELement.textContent = msg.name;
 
-            var msgTextElement = document.createElement("p");
-            msgTextElement.textContent = msg.text;
-            var sentMsgElement = document.createElement("div");
-            sentMsgElement.appendChild(msgTextElement);
-            sentMsgElement.className = "sent_msg";
-            var outgoingMsgElement = document.createElement("div");
-            outgoingMsgElement.appendChild(sentMsgElement);
-            outgoingMsgElement.className = "outgoing_msg";
-            document.getElementById("msg_history").appendChild(msgElement);
-        });
-    }
+    //         var msgTextElement = document.createElement("p");
+    //         msgTextElement.textContent = msg.text;
+    //         var sentMsgElement = document.createElement("div");
+    //         sentMsgElement.appendChild(msgTextElement);
+    //         sentMsgElement.className = "sent_msg";
+    //         var outgoingMsgElement = document.createElement("div");
+    //         outgoingMsgElement.appendChild(sentMsgElement);
+    //         outgoingMsgElement.className = "outgoing_msg";
+    //         document.getElementById("msg_history").appendChild(msgElement);
+    //     });
+    // }
 
-    beginListening();
+    // beginListening();
 
 })
