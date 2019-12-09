fea858701e6c20495909aea1314f6b2e6929ffa0
Juila Li <lijulia@wustl.edu>
2019-12-09 15:10:37 -0600

    updated messaging

--- api/public/js/messaging.js
+++ api/public/js/messaging.js
@@ -1,35 +1,39 @@
-var myFirebase = new Firebase('https://fir-messaging-app-86905.firebaseio.com/');
+$(document).ready(function () {
 
-var textInput = document.querySelector(".write_msg");
-var postButton = document.querySelector(".msg_send_btn");
+    var myFirebase = new Firebase('https://fir-messaging-app-86905.firebaseio.com/');
 
-postButton.addEventListener("click", function() {
-    var msgText = textInput.value;
-    myFirebase.set(msgText);
-    textInput.value = "";
+    var textInput = document.querySelector(".write_msg");
+    var postButton = document.querySelector(".msg_send_btn");
 
-    var testUser = "test";
+    postButton.addEventListener("click", function () {
+        var msgText = textInput.value;
+        myFirebase.set(msgText);
+        textInput.value = "";
 
-    myFirebase.push({name: testUser, sender_id: "1234", text: msgText});
-});
+        var testUser = "test";
 
-var beginListening = function() {
-    myFirebase.on('child_added', function(snapshot) {
-        var msg = snapshot.val();
-
-        // var msgUsernameElement = document.createELement("b");
-        // msgUsernameELement.textContent = msg.name;
-
-        var msgTextElement = document.createElement("p");
-        msgTextElement.textContent = msg.text;
-        var sentMsgElement = document.createElement("div");
-        sentMsgElement.appendChild(msgTextElement);
-        sentMsgElement.className = "sent_msg";
-        var outgoingMsgElement = document.createElement("div");
-        outgoingMsgElement.appendChild(sentMsgElement);
-        outgoingMsgElement.className = "outgoing_msg";
-        document.getElementById("msg_history").appendChild(msgElement);
+        myFirebase.push({ name: testUser, sender_id: "1234", text: msgText });
     });
-}
 
-beginListenging();
+    var beginListening = function () {
+        myFirebase.on('child_added', function (snapshot) {
+            var msg = snapshot.val();
+
+            // var msgUsernameElement = document.createELement("b");
+            // msgUsernameELement.textContent = msg.name;
+
+            var msgTextElement = document.createElement("p");
+            msgTextElement.textContent = msg.text;
+            var sentMsgElement = document.createElement("div");
+            sentMsgElement.appendChild(msgTextElement);
+            sentMsgElement.className = "sent_msg";
+            var outgoingMsgElement = document.createElement("div");
+            outgoingMsgElement.appendChild(sentMsgElement);
+            outgoingMsgElement.className = "outgoing_msg";
+            document.getElementById("msg_history").appendChild(msgElement);
+        });
+    }
+
+    beginListening();
+
+})
