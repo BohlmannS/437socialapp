3a5b51baf5a416e291f0038b5b817a07ceacad3c
Juila Li <lijulia@wustl.edu>
2019-12-09 13:37:20 -0600

    updated messaging test

---
+++ api/public/js/messaging.js
@@ -0,0 +1,35 @@
+var myFirebase = new Firebase('https://fir-messaging-app-86905.firebaseio.com/');
+
+var textInput = document.querySelector(".write_msg");
+var postButton = document.querySelector(".msg_send_btn");
+
+postButton.addEventListener("click", function() {
+    var msgText = textInput.value;
+    myFirebase.set(msgText);
+    textInput.value = "";
+
+    var testUser = "test";
+
+    myFirebase.push({name: testUser, sender_id: "1234", text: msgText});
+});
+
+var beginListening = function() {
+    myFirebase.on('child_added', function(snapshot) {
+        var msg = snapshot.val();
+
+        // var msgUsernameElement = document.createELement("b");
+        // msgUsernameELement.textContent = msg.name;
+
+        var msgTextElement = document.createElement("p");
+        msgTextElement.textContent = msg.text;
+        var sentMsgElement = document.createElement("div");
+        sentMsgElement.appendChild(msgTextElement);
+        sentMsgElement.className = "sent_msg";
+        var outgoingMsgElement = document.createElement("div");
+        outgoingMsgElement.appendChild(sentMsgElement);
+        outgoingMsgElement.className = "outgoing_msg";
+        document.getElementById("msg_history").appendChild(msgElement);
+    });
+}
+
+beginListenging();
