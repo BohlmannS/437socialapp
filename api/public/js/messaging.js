263cbcfd88810f1e2130d213582dc823f8d5c5e8
Juila Li <lijulia@wustl.edu>
2019-12-10 07:11:37 -0600

    test

--- api/public/js/messaging.js
+++ api/public/js/messaging.js
@@ -87,7 +87,7 @@ $(document).ready(function () {
             console.log(chatLog);
         }
 
-        const chatLogObject = firebase.database().ref().child('chats');
+        const chatLogObject = firebase.database().ref();
         chatLogObject.once('value').then(function (snapshot) {
             snapshot.forEach(function (child) {
                 // var exists = true;
