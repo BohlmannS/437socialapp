526520ead97de5b560dbd330f8ddad8b853f5341
Juila Li <lijulia@wustl.edu>
2019-12-10 03:45:37 -0600

    order messed up ahhh

--- api/public/js/messaging.js
+++ api/public/js/messaging.js
@@ -66,7 +66,7 @@ $(document).ready(function () {
         const preObject = document.getElementById('object');
         const dbRefObject = firebase.database().ref().child('chats').child(chatLog);
 
-        dbRefObject.orderByChild("text").on("child_added", function (snapshot) {
+        dbRefObject.on("child_added", function (snapshot) {
             console.log(snapshot.key + " was " + snapshot.val().sender_id + " meters tall");
 
             // var sentMessage = document.createElement("p");
