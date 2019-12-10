d91a8fb6ac5b1a015c8d34d112aeb229f64cc5ef
Juila Li <lijulia@wustl.edu>
2019-12-10 06:57:11 -0600

    test

--- api/public/js/messaging.js
+++ api/public/js/messaging.js
@@ -102,7 +102,7 @@ $(document).ready(function () {
         });
         if(exists == false ){
             chatLogObject.push({
-                chatLog: null;
+                chatLog: null
             })
         }
 
