1def9f19caf7e431df06888b7b64e43d623d2205
Juila Li <lijulia@wustl.edu>
2019-12-09 22:53:54 -0600

    changed childNodes to children

--- api/public/js/messaging.js
+++ api/public/js/messaging.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
         classname[i].addEventListener("click", function () {
             console.log("friend clicked");
             // console.log(classname[i].querySelector(".friend").text);
-            console.log(classname[i].childNodes[0].childNodes[0].childNodes[0].text);
+            console.log(classname[i].children[0].children[0].children[0].text);
         });
     }
 
