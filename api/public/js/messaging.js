7eb585160fcac994ce567d45301d6f255045682d
Juila Li <lijulia@wustl.edu>
2019-12-09 23:21:59 -0600

    get friends

--- api/public/js/messaging.js
+++ api/public/js/messaging.js
@@ -48,7 +48,7 @@ $(document).ready(function () {
         classname[i].addEventListener("click", function () {
             console.log("friend clicked");
             // console.log(classname[i].querySelector(".friend").text);
-            console.log(friends[i].text);
+            console.log(friends[i].textContent);
         });
     }
 
