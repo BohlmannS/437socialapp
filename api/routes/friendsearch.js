7ede7628be02e4fda1897457656d8015e1d599d6
BohlmannS <bohlmanns@wustl.edu>
2019-12-10 21:41:28 +0000

    friends page desktop

--- api/routes/friendsearch.js
+++ api/routes/friendsearch.js
@@ -17,7 +17,7 @@ connection.connect(function(err) {
 router.post('/', function(req, res) {
 //	console.log(req.body);
         let uid = req.body.uid;
-    let sql = 'select username, first_name, last_name from users where uid <> ' + uid;
+    let sql = 'select uid, username, first_name, last_name from users where uid <> ' + uid;
     //console.log('sql: ',sql);
     connection.query(sql, function(err, rows, fields){
         if(err){
@@ -26,9 +26,24 @@ router.post('/', function(req, res) {
                 return;
         }
     //console.log('This is rows: ', rows);
+    let sql2 = 'select greater from friends where lower=' + req.body.uid + ' union select lower from friends where greater='+req.body.uid;
+	connection.query(sql2, function(err2, rows2, f2){
+	let response = [];	
+	let ids= [];
+	rows2.forEach(function(e){
+		for(var prop1 in e){
+			ids.push(e[prop1]);
+		}
+	})
+	rows.forEach(function(e){
+		if(ids.includes(e.uid)){}
+		else{
+			response.push({username: e.username, first_name: e.first_name, last_name: e.last_name});
+		}
+	})
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(rows));
-    })
+    res.end(JSON.stringify(response));
+    })})
 
 });
 
