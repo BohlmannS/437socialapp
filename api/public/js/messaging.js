8391fbf8f35fdeef7bf33e0f39d8100a77d52f4d
Juila Li <lijulia@wustl.edu>
2019-12-10 02:59:35 -0600

    chatS

--- api/public/js/messaging.js
+++ api/public/js/messaging.js
@@ -58,7 +58,7 @@ $(document).ready(function () {
         }
 
         const preObject = document.getElementById('object');
-        const dbRefObject = firebase.database().ref().child('chat').child(chatLog);
+        const dbRefObject = firebase.database().ref().child('chats').child(chatLog);
 
         dbRefObject.orderByChild("text").on("child_added", function (snapshot) {
             console.log(snapshot.key + " was " + snapshot.val().sender_id + " meters tall");
