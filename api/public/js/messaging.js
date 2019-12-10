e29ce6fe2245dd2b2b40ad5049005a70994f6881
Juila Li <lijulia@wustl.edu>
2019-12-10 12:53:51 -0600

    try again

--- api/public/js/messaging.js
+++ api/public/js/messaging.js
@@ -266,9 +266,9 @@ $(document).ready(function () {
             text: textInput.value
         }).then(() => {
             dbRefObject.once("child_added").then(function (snapshot) {
-                // dbRefObject.on("child_added", function (snapshot) {
+            // dbRefObject.on("child_added", function (snapshot) {
                 // console.log(snapshot.key + " was " + snapshot.val().sender_id + " meters tall");
-
+    
                 snapshot.forEach(function (child) {
 
                     var sentMessage = document.createElement("p");
@@ -283,26 +283,35 @@ $(document).ready(function () {
                     //     console.log(snapshot.child("text").val());
                     console.log(child.val().text);
                     preObject.appendChild(sentMessage);
-
-                    // console.log(snapshot.child("text").val());
-
-                    // var sentMessage = document.createElement("p");
-                    // // if (snapshot.child("sender_id").val() == myUsername) {
-                    // //     sentMessage.textContent = (myUsername + ": " + snapshot.child("text").val());
-                    // // }
-                    // // else {
-                    // //     sentMessage.textContent = (friendUsername + ": " + snapshot.child("text").val());
-                    // // }
-                    // sentMessage.textContent = snapshot.child("text").val();
-                    // sentMessage.className = "visible_messages";
-                    // // console.log(snapshot.child("text").val());
-                    // preObject.appendChild(sentMessage);
-                })
+                });
+                // console.log(snapshot.child("text").val());
+    
+                // // var sentMessage = document.createElement("p");
+                // // sentMessage.textContent = snapshot.val().name;
+                // // preObject.appendChild(sentMessage);
+    
+                // // if (snapshot.val().sender_id == myUsername) {
+                // //     var sentMessage = document.createElement("p");
+                // //     sentMessage.textContent = snapshot.val().text;
+                // //     preObject.appendChild(sentMessage);
+                // // }
+    
+                // var sentMessage = document.createElement("p");
+                // // if (snapshot.child("sender_id").val() == myUsername) {
+                // //     sentMessage.textContent = (myUsername + ": " + snapshot.child("text").val());
+                // // }
+                // // else {
+                // //     sentMessage.textContent = (friendUsername + ": " + snapshot.child("text").val());
+                // // }
+                // sentMessage.textContent = snapshot.child("text").val();
+                // sentMessage.className = "visible_messages";
+                // // console.log(snapshot.child("text").val());
+                // preObject.appendChild(sentMessage);
             });
         })
 
         // dbRefObject.once('value').then(function (snapshot) {
-
+        
         console.log("send button clicked");
         // var testUser = "test";
 
