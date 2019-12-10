946a6a1f3b2196fa09cab2ed04c8a24dfbc1a78d
Juila Li <lijulia@wustl.edu>
2019-12-10 06:17:06 -0600

    test

--- api/public/js/messaging.js
+++ api/public/js/messaging.js
@@ -93,7 +93,7 @@ $(document).ready(function () {
 
         dbRefObject.once('value').then(function (snapshot) {
             if (snapshot.val()) {
-                console.log(snapshot.val());
+                console.log(snapshot.val().text);
             } else {
                 console.log('/whatever/whateverProperty node does not exist!');
             }
